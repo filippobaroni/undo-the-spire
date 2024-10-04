5716a02184fc9d4a57935fe9ee9196efd0f2d7f6
Delfad0r <filippo.gianni.baroni@gmail.com>
2024-10-04 23:28:30 +0100

    Hopefully fixed the bug where additional rewards from event fights or mugger were not kept track of,

---
+++ src/main/java/undobutton/patches/RoomStatePatches.java
@@ -0,0 +1,50 @@
+package undobutton.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.map.MapRoomNode;
+import com.megacrit.cardcrawl.rewards.RewardItem;
+import savestate.MapRoomNodeState;
+import undobutton.UndoButtonMod;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class RoomStatePatches {
+    @SpirePatch(clz = MapRoomNodeState.class, method = SpirePatch.CLASS)
+    public static class RewardsField {
+        public static SpireField<ArrayList<RewardItem>> rewards = new SpireField<>(() -> null);
+    }
+
+    @SpirePatch(clz = MapRoomNodeState.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {MapRoomNode.class})
+    public static class ConstructorPatch {
+        @SpirePostfixPatch
+        public static void PopulateRewards(MapRoomNodeState __instance, MapRoomNode node) {
+            RewardsField.rewards.set(__instance, node.room.rewards.stream().map(r -> copyRewardItem(r)).collect(Collectors.toCollection(ArrayList::new)));
+        }
+    }
+
+    @SpirePatch(clz = MapRoomNodeState.class, method = "loadMapRoomNode")
+    public static class LoadPatch {
+        @SpirePostfixPatch
+        public static MapRoomNode Postfix(MapRoomNode __result, MapRoomNodeState __instance) {
+            __result.room.rewards = RewardsField.rewards.get(__instance).stream().map(r -> copyRewardItem(r)).collect(Collectors.toCollection(ArrayList::new));
+            return __result;
+        }
+    }
+
+    public static RewardItem copyRewardItem(RewardItem rewardItem) {
+        switch (rewardItem.type) {
+            case RELIC:
+                return new RewardItem(rewardItem.relic.makeCopy());
+            case GOLD:
+                return new RewardItem(rewardItem.goldAmt);
+            case STOLEN_GOLD:
+                return new RewardItem(rewardItem.goldAmt, true);
+            default:
+                UndoButtonMod.logger.error("Unsupported reward type: {}", rewardItem.type);
+                throw new UnsupportedOperationException("Unsupported reward type: " + rewardItem.type);
+        }
+    }
+}
