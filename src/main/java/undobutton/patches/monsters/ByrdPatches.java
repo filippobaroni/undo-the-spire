1d287b5215f605f267fd44d6e4090de59c14bca6
Delfad0r <filippo.gianni.baroni@gmail.com>
2024-10-13 00:55:21 +0100

    Fixed Byrds intent on undoing when grounded.

--- src/main/java/undobutton/patches/monsters/ByrdPatches.java
+++ src/main/java/undobutton/patches/monsters/ByrdPatches.java
@@ -1,7 +1,11 @@
 package undobutton.patches.monsters;
 
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.math.MathUtils;
+import com.esotericsoftware.spine.AnimationState;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import savestate.monsters.city.ByrdState;
 
@@ -10,7 +14,12 @@ public class ByrdPatches {
     public static class loadMonsterPatch {
         @SpirePostfixPatch
         public static AbstractMonster setState(AbstractMonster __result, ByrdState __instance, boolean ___isFlying) {
-            __result.changeState(___isFlying ? "FLYING" : "GROUNDED");
+            if (!___isFlying) {
+                ReflectionHacks.privateMethod(AbstractCreature.class, "loadAnimation", String.class, String.class, float.class).invoke(__result, "images/monsters/theCity/byrd/grounded.atlas", "images/monsters/theCity/byrd/grounded.json", 1.0F);
+                AnimationState.TrackEntry e = __result.state.setAnimation(0, "idle", true);
+                e.setTime(e.getEndTime() * MathUtils.random());
+                ReflectionHacks.privateMethod(AbstractMonster.class, "updateHitbox", float.class, float.class, float.class, float.class).invoke(__result, 10.0F, -50.0F, 240.0F, 180.0F);
+            }
             return __result;
         }
     }
