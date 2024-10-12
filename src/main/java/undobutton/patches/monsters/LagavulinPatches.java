dcacfafd15333a5805da5617591d741caf4bb3bc
Delfad0r <filippo.gianni.baroni@gmail.com>
2024-10-12 17:41:47 +0100

    Restore correct music in Lagavulin fight after undoing.

--- src/main/java/undobutton/patches/monsters/LagavulinPatches.java
+++ src/main/java/undobutton/patches/monsters/LagavulinPatches.java
@@ -1,8 +1,14 @@
 package undobutton.patches.monsters;
 
+import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInstrumentPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.exordium.Lagavulin;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 import javassist.expr.NewExpr;
@@ -12,7 +18,7 @@ public class LagavulinPatches {
     @SpirePatch(clz = LagavulinState.class, method = "loadMonster")
     public static class loadMonsterPatch {
         @SpireInstrumentPatch
-        public static ExprEditor modifyLoadMonster() {
+        public static ExprEditor fixSetAsleep() {
             return new ExprEditor() {
                 @Override
                 public void edit(NewExpr e) throws CannotCompileException {
@@ -22,5 +28,28 @@ public class LagavulinPatches {
                 }
             };
         }
+
+        @SpirePostfixPatch
+        public static AbstractMonster fixMusic(AbstractMonster __result, LagavulinState __instance, boolean ___isOutTriggered) {
+            boolean currentlyOpen = false;
+            AbstractRoom currentRoom = AbstractDungeon.getCurrRoom();
+            if (currentRoom != null) {
+                if (currentRoom.monsters != null && currentRoom.monsters.getMonster("Lagavulin") != null) {
+                    currentlyOpen = ReflectionHacks.getPrivate(currentRoom.monsters.getMonster("Lagavulin"), Lagavulin.class, "isOutTriggered");
+                }
+            }
+            if (currentlyOpen && !___isOutTriggered) {
+                CardCrawlGame.music.silenceTempBgmInstantly();
+                CardCrawlGame.music.unsilenceBGM();
+                AbstractDungeon.scene.fadeInAmbiance();
+            }
+            if (!currentlyOpen && ___isOutTriggered) {
+                CardCrawlGame.music.unsilenceBGM();
+                AbstractDungeon.scene.fadeOutAmbiance();
+                CardCrawlGame.music.playTempBgmInstantly("ELITE");
+            }
+            return __result;
+        }
+
     }
 }
