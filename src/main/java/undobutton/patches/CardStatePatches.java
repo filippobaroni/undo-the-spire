72e3dbb25276352e666b502b10c3aa8985adffad
Delfad0r <filippo.gianni.baroni@gmail.com>
2024-10-14 01:04:19 +0100

    Cards go back to their original position after undoing (only relevant for cards being played during a card selection screen).

--- src/main/java/undobutton/patches/CardStatePatches.java
+++ src/main/java/undobutton/patches/CardStatePatches.java
@@ -8,19 +8,22 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import savestate.CardState;
 
 public class CardStatePatches {
-    // Patch CardState to store the name and description of the card.
     @SpirePatch(clz = CardState.class, method = SpirePatch.CLASS)
-    public static class CardStateExtraFields {
+    public static class ExtraFields {
         public static SpireField<String> name = new SpireField<>(() -> null);
         public static SpireField<String> description = new SpireField<>(() -> null);
+        public static SpireField<Float> target_x = new SpireField<>(() -> 0.0F);
+        public static SpireField<Float> target_y = new SpireField<>(() -> 0.0F);
     }
 
     @SpirePatch(clz = CardState.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {AbstractCard.class})
     public static class CardStateConstructorPatch {
         @SpirePostfixPatch
         public static void Postfix(CardState __instance, AbstractCard card) {
-            CardStatePatches.CardStateExtraFields.name.set(__instance, card.name);
-            CardStatePatches.CardStateExtraFields.description.set(__instance, card.rawDescription);
+            ExtraFields.name.set(__instance, card.name);
+            ExtraFields.description.set(__instance, card.rawDescription);
+            ExtraFields.target_x.set(__instance, card.target_x);
+            ExtraFields.target_y.set(__instance, card.target_y);
         }
     }
 
@@ -28,9 +31,13 @@ public class CardStatePatches {
     public static class LoadCardPatch {
         @SpirePostfixPatch
         public static AbstractCard Postfix(AbstractCard __result, CardState __instance) {
-            __result.name = CardStatePatches.CardStateExtraFields.name.get(__instance);
-            __result.rawDescription = CardStatePatches.CardStateExtraFields.description.get(__instance);
+            __result.name = ExtraFields.name.get(__instance);
+            __result.rawDescription = ExtraFields.description.get(__instance);
             __result.initializeDescription();
+            __result.target_x = ExtraFields.target_x.get(__instance);
+            __result.target_y = ExtraFields.target_y.get(__instance);
+            __result.current_x = __result.target_x;
+            __result.current_y = __result.target_y;
             return __result;
         }
     }
