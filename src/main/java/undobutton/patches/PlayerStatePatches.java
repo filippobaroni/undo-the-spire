a4dd387e3c10da1864f3aa09381d88671934aefd
Delfad0r <filippo.gianni.baroni@gmail.com>
2024-10-24 05:13:32 +0100

    Correctly keep track of orbs channeled this turn.

--- src/main/java/undobutton/patches/PlayerStatePatches.java
+++ src/main/java/undobutton/patches/PlayerStatePatches.java
@@ -1,19 +1,38 @@
 package undobutton.patches;
 
 import basemod.ReflectionHacks;
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInstrumentPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 import savestate.PlayerState;
+import savestate.orbs.OrbState;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 
 public class PlayerStatePatches {
+    @SpirePatch(clz = PlayerState.class, method = SpirePatch.CLASS)
+    public static class ExtraFields {
+        public static SpireField<ArrayList<OrbState>> orbsChanneledThisTurn = new SpireField<>(ArrayList::new);
+    }
+
+    @SpirePatch(clz = PlayerState.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {AbstractPlayer.class})
+    public static class ConstructorPatch {
+        @SpirePostfixPatch
+        public static void saveOrbsChanneledThisTurn(PlayerState __instance, AbstractPlayer player) {
+            ExtraFields.orbsChanneledThisTurn.set(__instance, AbstractDungeon.actionManager.orbsChanneledThisTurn.stream().map(OrbState::forOrb).collect(Collectors.toCollection(ArrayList::new)));
+        }
+    }
+
     @SpirePatch(clz = PlayerState.class, method = "loadPlayer")
     public static class LoadPlayerPatch {
         // Patch PlayerState.loadPlayer to set EnergyPanel.totalCount instead of calling setEnergy.
@@ -36,6 +55,13 @@ public class PlayerStatePatches {
             __result.healthBarUpdatedEvent();
             ReflectionHacks.setPrivate(__result, AbstractCreature.class, "healthBarWidth", ReflectionHacks.getPrivate(__result, AbstractCreature.class, "targetHealthBarWidth"));
             ReflectionHacks.setPrivate(__result, AbstractCreature.class, "healthBarAnimTimer", 0.0F);
+            return __result;
+        }
+
+        // Load the orbs channeled this turn
+        @SpirePostfixPatch
+        public static AbstractPlayer loadOrbsChanneledThisTurn(AbstractPlayer __result, PlayerState __instance) {
+            AbstractDungeon.actionManager.orbsChanneledThisTurn = ExtraFields.orbsChanneledThisTurn.get(__instance).stream().map(OrbState::loadOrb).collect(Collectors.toCollection(ArrayList::new));
             return __result;
         }
     }
