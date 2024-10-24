658b0080e1c7bdd2a9b8efb840fc18d4bfa5793c
Delfad0r <filippo.gianni.baroni@gmail.com>
2024-10-24 05:32:26 +0100

    Added StatsTracker to keep track of stats for achievements and final score.

---
+++ src/main/java/undobutton/StatsTracker.java
@@ -0,0 +1,28 @@
+package undobutton;
+
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import undobutton.util.MakeUndoable;
+
+@MakeUndoable(statetype = StatsTracker.class)
+public class StatsTracker {
+    public static class StatsState {
+        public final int goldGained;
+        public final int damageReceivedThisCombat;
+        public final int hpLossThisCombat;
+
+        public StatsState() {
+            goldGained = CardCrawlGame.goldGained;
+            damageReceivedThisCombat = GameActionManager.damageReceivedThisCombat;
+            hpLossThisCombat = GameActionManager.hpLossThisCombat;
+        }
+    }
+
+    public static StatsState save() {
+        return new StatsState();
+    }
+
+    public static void load(StatsState state) {
+        CardCrawlGame.goldGained = state.goldGained;
+    }
+}
