6d144f79c7156dae1d21ee97f610eebc7d1fa1b4
Delfad0r <filippo.gianni.baroni@gmail.com>
2024-10-24 04:30:57 +0100

    Wasn't actually that long, just added a patch for Exhume, which is probably not even needed.

--- src/main/java/undobutton/patches/CardLoadingPatches.java
+++ src/main/java/undobutton/patches/CardLoadingPatches.java
@@ -3,12 +3,17 @@ package undobutton.patches;
 import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.unique.ExhumeAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardQueueItem;
 import savestate.CardQueueItemState;
+import savestate.actions.ExhumeActionState;
 import savestate.actions.UseCardActionState;
 import savestate.fastobjects.actions.UpdateOnlyUseCardAction;
 import undobutton.GameState;
+
+import java.util.ArrayList;
 
 public class CardLoadingPatches {
     @SpirePatch(clz = CardQueueItemState.class, method = "loadItem")
@@ -35,6 +40,17 @@ public class CardLoadingPatches {
                 if (c != null) {
                     ReflectionHacks.setPrivate(__result, UpdateOnlyUseCardAction.class, "targetCard", c);
                 }
+            });
+            return __result;
+        }
+    }
+
+    @SpirePatch(clz = ExhumeActionState.class, method = "loadCurrentAction", paramtypez = {})
+    public static class ExhumeActionPatch {
+        @SpirePostfixPatch
+        public static AbstractGameAction linkCard(AbstractGameAction __result, ExhumeActionState __instance) {
+            GameState.addPostLoadRunner((info) -> {
+                ((ArrayList<AbstractCard>) ReflectionHacks.getPrivate(__result, ExhumeAction.class, "exhumes")).replaceAll(info::getAbstractCard);
             });
             return __result;
         }
