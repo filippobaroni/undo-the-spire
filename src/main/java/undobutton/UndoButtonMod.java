aa9cf43983fd8dafdf60931d8cf812e3bba448f4
Delfad0r <filippo.gianni.baroni@gmail.com>
2024-10-27 18:36:14 +0000

    The mod tries its best to save states for unknown powers (assuming they are completely determined by their class and their amount).

--- src/main/java/undobutton/UndoButtonMod.java
+++ src/main/java/undobutton/UndoButtonMod.java
@@ -45,7 +45,7 @@ import static java.lang.reflect.Modifier.isStatic;
 @SpireInitializer
 @SpireSideload(modIDs = {"SaveStateMod"})
 public class UndoButtonMod implements EditStringsSubscriber, PostInitializeSubscriber, OnStartBattleSubscriber, RenderSubscriber, PostUpdateSubscriber, PrePotionUseSubscriber {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     private static final String resourcesFolder = checkResourcesPath();
     private static final String defaultLanguage = "eng";
